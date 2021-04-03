608235151fabd622eb4e06e13721c0e86d192b60
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-03 16:48:27 -0300

    Adds missing routes

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const routes = express.Router();
 
+const basePath = `${__dirname}/views`;
+
 routes.get("/", (request, response) => {
-    return response.sendFile(`${__dirname}/views/index.html`);
+    return response.sendFile(`${basePath}/index.html`);
+});
+
+routes.get("/job", (request, response) => {
+    return response.sendFile(`${basePath}/job.html`);
+});
+
+routes.get("/job/edit", (request, response) => {
+    return response.sendFile(`${basePath}/job-edit.html`);
+});
+
+routes.get("/profile", (request, response) => {
+    return response.sendFile(`${basePath}/profile.html`);
 });
 
 module.exports = routes;
