8d336e00447c8495dd2675644f850ebc2cf68b0c
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-03 17:13:03 -0300

    Changes routes configuration to render ejs

--- src/routes.js
+++ src/routes.js
@@ -2,22 +2,22 @@ const express = require("express");
 
 const routes = express.Router();
 
-const basePath = `${__dirname}/views`;
+const basePath = `${__dirname}/views/`;
 
 routes.get("/", (request, response) => {
-    return response.sendFile(`${basePath}/index.html`);
+    return response.render(`${basePath}index`);
 });
 
 routes.get("/job", (request, response) => {
-    return response.sendFile(`${basePath}/job.html`);
+    return response.render(`${basePath}job`);
 });
 
 routes.get("/job/edit", (request, response) => {
-    return response.sendFile(`${basePath}/job-edit.html`);
+    return response.render(`${basePath}job-edit`);
 });
 
 routes.get("/profile", (request, response) => {
-    return response.sendFile(`${basePath}/profile.html`);
+    return response.render(`${basePath}profile`);
 });
 
 module.exports = routes;
