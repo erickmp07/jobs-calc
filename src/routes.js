30365716e2cbaa6ef3fd54e83a5559960d7820aa
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-10 01:59:12 -0300

    Job refactoring on the back-end to Object Literals

--- src/routes.js
+++ src/routes.js
@@ -14,60 +14,70 @@ const profile = {
     "hour-value": 75
 };
 
+const Job = {
+    data: [],
+    controllers: {
+        index(request, response) {
+            const updatedJobs = Job.data.map((job) => {
+                const remainingDays = Job.services.remainingDays(job);
+                const status = remainingDays <= 0
+                    ? "done"
+                    : "progress";
+
+                return {
+                    ...job,
+                    remaining,
+                    status,
+                    budget: profile["hour-value"] * job["total-hours"]
+                };
+            });
+
+            return response.render(`${basePath}index`, { jobs: updatedJobs });
+        },
+        list(request, response) {
+            return response.render(`${basePath}job`);
+        },
+        save(request, response) {
+            const jobId = Job.data[Job.data.length - 1]?.id + 1 || 1;
+
+            jobs.push({
+                id: jobId,
+                name: request.body.name,
+                "daily-hours": request.body["daily-hours"],
+                "total-hours": request.body["total-hours"],
+                createdAt: Date.now()
+            });
+
+            return response.redirect("/");
+        }
+    },
+    services: {
+        remainingDays(job) {
+            const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+
+            const createdDate = new Date(job.createdAt);
+
+            const dueDay = createdDate.getDate() + Number(remainingDays);
+            const dueDateInMs = createdDate.setDate(dueDay);
+
+            const timeDiffInMs = dueDateInMs - Date.now();
+
+            const dayInMs = 1000 * 60 * 60 * 24;
+
+            const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+
+            return dayDiff;
+        }
+    }
+};
+
 const jobs = [];
 
-function remainingDays(job) {
-    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+routes.get("/", Job.controllers.index);
 
-    const createdDate = new Date(job.createdAt);
+routes.get("/job", Job.controllers.list);
 
-    const dueDay = createdDate.getDate() + Number(remainingDays);
-    const dueDateInMs = createdDate.setDate(dueDay);
-
-    const timeDiffInMs = dueDateInMs - Date.now();
-
-    const dayInMs = 1000 * 60 * 60 * 24;
-
-    const dayDiff = Math.floor(timeDiffInMs / dayInMs);
-
-    return dayDiff;
-}
-
-routes.get("/", (request, response) => {
-    const updatedJobs = jobs.map((job) => {
-        const remainingDays = remainingDays(job);
-        const status = remainingDays <= 0 
-            ? "done"
-            : "progress";
-
-        return {
-            ...job,
-            remaining,
-            status,
-            budget: profile["hour-value"] * job["total-hours"]
-        };
-    });
-    
-    return response.render(`${basePath}index`, { jobs: updatedJobs });
-});
-
-routes.get("/job", (request, response) => {
-    return response.render(`${basePath}job`);
-});
-
-routes.post("/job", (request, response) => {
-    const jobId = jobs[jobs.length - 1]?.id + 1 || 1;
-    
-    jobs.push({
-        id: jobId,
-        name: request.body.name,
-        "daily-hours": request.body["daily-hours"],
-        "total-hours": request.body["total-hours"],
-        createdAt: Date.now()
-    });
-
-    return response.redirect("/");
-});
+routes.post("/job", Job.controllers.save);
 
 routes.get("/job/edit", (request, response) => {
     return response.render(`${basePath}job-edit`);
