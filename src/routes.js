8883d59947278854fde84d58693127b44da35cfb
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-10 01:07:18 -0300

    Calculates the remaining days of jobs in the index

--- src/routes.js
+++ src/routes.js
@@ -10,13 +10,45 @@ const profile = {
     "monthly-budget": 3000,
     "hours-per-day": 5,
     "days-per-week": 5,
-    "vacation-per-year": 4
+    "vacation-per-year": 4,
+    "hour-value": 75
 };
 
 const jobs = [];
 
+function remainingDays(job) {
+    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+
+    const createdDate = new Date(job.createdAt);
+
+    const dueDay = createdDate.getDate() + Number(remainingDays);
+    const dueDateInMs = createdDate.setDate(dueDay);
+
+    const timeDiffInMs = dueDateInMs - Date.now();
+
+    const dayInMs = 1000 * 60 * 60 * 24;
+
+    const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+
+    return dayDiff;
+}
+
 routes.get("/", (request, response) => {
-    return response.render(`${basePath}index`, { jobs });
+    const updatedJobs = jobs.map((job) => {
+        const remainingDays = remainingDays(job);
+        const status = remainingDays <= 0 
+            ? "done"
+            : "progress";
+
+        return {
+            ...job,
+            remaining,
+            status,
+            budget: profile["hour-value"] * job["total-hours"]
+        };
+    });
+    
+    return response.render(`${basePath}index`, { jobs: updatedJobs });
 });
 
 routes.get("/job", (request, response) => {
